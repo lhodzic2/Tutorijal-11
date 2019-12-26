66eedc4348c7c33617087fee6eea371d9d7dbc32
lhodzic2 <lhodzic2@etf.unsa.ba>
2019-12-26 23:38:15 +0100

    Finalna verzija

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -7,14 +7,26 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Gradovi su:\n" + ispisiGradove());
-//        glavniGrad();
+        glavniGrad();
+    }
+
+    public static void glavniGrad() {
+        System.out.println("Unesite drzavu\n");
+        Scanner unos = new Scanner(System.in);
+        String d = unos.nextLine();
+        Grad g = GeografijaDAO.getInstance().glavniGrad(d);
+        if (g == null) {
+            System.out.println("Nepostojeca drzava");
+            return;
+        }
+        System.out.println("Glavni grad drzave " + d +" "+g.getNaziv() );
     }
 
     public static String ispisiGradove() {
         ArrayList<Grad> lista = GeografijaDAO.getInstance().gradovi();
         String s = "";
         for ( Grad g : lista) {
-            s = s + g.getNaziv() + "(" + g.getDrzava().getNaziv() + ") - " + g.getBrojStanovnika() + "\n";
+            s = s + g.getNaziv() + " (" + g.getDrzava().getNaziv() + ") - " + g.getBrojStanovnika() + "\n";
         }
         return s;
     }
