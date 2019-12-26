235f3d94bed1e407f59f251bc831a765e2cede58
lhodzic2 <lhodzic2@etf.unsa.ba>
2019-12-26 23:26:14 +0100

    Prolaze testovi

--- baza.db.sql
+++ baza.db.sql
@@ -1,4 +1,11 @@
 BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "drzava" (
+	"id"	INTEGER,
+	"naziv"	TEXT,
+	"glavni_grad"	INTEGER,
+	PRIMARY KEY("id"),
+	FOREIGN KEY("glavni_grad") REFERENCES "grad"("id")
+);
 CREATE TABLE IF NOT EXISTS "grad" (
 	"id"	INTEGER,
 	"naziv"	TEXT,
@@ -7,19 +14,12 @@ CREATE TABLE IF NOT EXISTS "grad" (
 	PRIMARY KEY("id"),
 	FOREIGN KEY("drzava") REFERENCES "drzava"("id")
 );
-CREATE TABLE IF NOT EXISTS "drzava" (
-	"id"	INTEGER,
-	"naziv"	TEXT,
-	"grad"	INTEGER,
-	PRIMARY KEY("id"),
-	FOREIGN KEY("grad") REFERENCES "grad"("id")
-);
-INSERT INTO "grad" VALUES (1,'Pariz',2200000,1);
-INSERT INTO "grad" VALUES (2,'London',8136000,2);
-INSERT INTO "grad" VALUES (3,'Beč',1868000,3);
-INSERT INTO "grad" VALUES (4,'Manchester',510746,2);
-INSERT INTO "grad" VALUES (5,'Graz',283869,3);
 INSERT INTO "drzava" VALUES (1,'Francuska',1);
 INSERT INTO "drzava" VALUES (2,'Velika Britanija',2);
 INSERT INTO "drzava" VALUES (3,'Austrija',3);
+INSERT INTO "grad" VALUES (1,'Pariz',2141000,1);
+INSERT INTO "grad" VALUES (2,'London',8908000,2);
+INSERT INTO "grad" VALUES (3,'Beč',1889000,3);
+INSERT INTO "grad" VALUES (4,'Manchester',510746,2);
+INSERT INTO "grad" VALUES (5,'Graz',439236,3);
 COMMIT;
--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -6,7 +6,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-//        System.out.println("Gradovi su:\n" + ispisiGradove());
+        System.out.println("Gradovi su:\n" + ispisiGradove());
 //        glavniGrad();
+    }
+
+    public static String ispisiGradove() {
+        ArrayList<Grad> lista = GeografijaDAO.getInstance().gradovi();
+        String s = "";
+        for ( Grad g : lista) {
+            s = s + g.getNaziv() + "(" + g.getDrzava().getNaziv() + ") - " + g.getBrojStanovnika() + "\n";
+        }
+        return s;
     }
 }
